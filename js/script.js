73a65df6920765aef3466c0a743cc0ed3b676da5
duncanfazio <duncan.fazio@gmail.com>
2025-11-19 14:43:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+// ===== LIGHTBOX =====
+function openLightbox(imgElement) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightbox.style.display = 'flex';
+    lightboxImg.src = imgElement.src;
+}
+
+function closeLightbox(event) {
+    if (event.target.id === 'lightbox' || event.target.className === 'close') {
+        const lightbox = document.getElementById('lightbox');
+        lightbox.style.display = 'none';
+    }
+}
 
